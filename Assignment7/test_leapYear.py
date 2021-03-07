f41d9b217eb02ab7aef0ca1ac2c91f0f142d394c
chitali <chitalib@gmail.com>
2021-03-07 15:54:47 -0800

    2. test 3 added

--- Assignment7/test_leapYear.py
+++ Assignment7/test_leapYear.py
@@ -5,6 +5,7 @@ class Testfullname(unittest.TestCase):
     def test_average(self):
         self.assertEqual(leapYear.calcYear(2020),"Leap year")
         self.assertEqual(leapYear.calcYear(2021),"Not a leap year")
+        self.assertEqual(leapYear.calcYear(1900),"Not a leap year")
      
 if __name__ ==  "__main__":
     unittest.main()
