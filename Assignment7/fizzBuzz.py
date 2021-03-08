8f4c58116f1f3a8740ae8edfc7eb658e6920b2fb
chitali <chitalib@gmail.com>
2021-03-07 17:11:30 -0800

    1. test 3 statsified

--- Assignment7/fizzBuzz.py
+++ Assignment7/fizzBuzz.py
@@ -1,7 +1,9 @@
 def fb():
     arr = []
     for x in range(1, 101):
-        if x % 3 == 0:
+        if x % 15 == 0:
+          arr.append('FizzBuzz')  
+        elif x % 3 == 0:
             arr.append('Fizz')
         elif x % 5 ==0:
             arr.append('Buzz')
