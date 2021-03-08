6089a1971c9eeac956f5b286f12c9345a319a3b8
chitali <chitalib@gmail.com>
2021-03-07 17:08:21 -0800

    1. test 2 satsified

--- Assignment7/fizzBuzz.py
+++ Assignment7/fizzBuzz.py
@@ -1,8 +1,10 @@
-def fb(i):
+def fb():
     arr = []
     for x in range(1, 101):
         if x % 3 == 0:
             arr.append('Fizz')
+        elif x % 5 ==0:
+            arr.append('Buzz')
         else:
             arr.append(x)
     return arr
