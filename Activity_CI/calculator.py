079b7fe28911f09cb907276d598be0d3402a8f86
chitali <chitalib@gmail.com>
2021-03-11 08:13:33 -0800

    virtual environment created outside the repo and calc file added

--- Activity_CI/calculator.py
+++ Activity_CI/calculator.py
@@ -1,15 +1,10 @@
-def add(num1, num2): 
-    try:
-        num1 = float(num1)
-        num2 = float(num2)
-    except ValueError:
-         return "Error"
-    return num1 + num2
+def add(a, b):
+    return a + b
 
-def subtract(num1, num2):
-    try:
-        num1 = float(num1)
-        num2 = float(num2)
-    except ValueError:
-         return "Error"
-    return num1 - num2
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
